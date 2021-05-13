8eb05b72e13d4ac9e4080920f5af61688115b3a6
vCamilx <72714741+vCamilx@users.noreply.github.com>
2021-05-13 14:36:38 -0500

    changes made with NMSHandler

--- version/v1_8_R3/src/main/java/org/neptuneclub/abilities/nms/NMSHandler_1_8_R3.java
+++ version/v1_8_R3/src/main/java/org/neptuneclub/abilities/nms/NMSHandler_1_8_R3.java
@@ -7,109 +7,138 @@ import team.unnamed.gui.core.item.type.ItemBuilder;
 
 public class NMSHandler_1_8_R3 implements NMSHandler {
 
-    public ItemStack grapplingHook() {
+    public ItemStack grapplingHook(String name, String... lore) {
         return ItemBuilder
                 .newBuilder(Material.FISHING_ROD)
-                .setName("Grappling Hook")
+                .setName(name)
+                .setLore(lore)
                 .build();
     }
 
-    public ItemStack rageBrick() {
+    public ItemStack rageBrick(String name, String... lore) {
+        return ItemBuilder
+                .newBuilder(Material.NETHER_BRICK_ITEM)
+                .setName(name)
+                .setLore(lore)
+                .build();
+    }
+
+
+    public ItemStack rocketLauncher(String name, String... lore) {
+        return ItemBuilder
+                .newBuilder(Material.FIREWORK)
+                .setName(name)
+                .setLore(lore)
+                .build();
+    }
+
+
+    public ItemStack swapperAxe(String name, String... lore) {
+        return ItemBuilder
+                .newBuilder(Material.GOLD_AXE)
+                .setName(name)
+                .setLore(lore)
+                .build();
+    }
+
+
+    public ItemStack timeWarp(String name, String... lore) {
+        return ItemBuilder
+                .newBuilder(Material.PAPER)
+                .setName(name)
+                .setLore(lore)
+                .build();
+    }
+
+
+    public ItemStack belchBomb(String name, String... lore) {
+        return ItemBuilder
+                .newBuilder(Material.SLIME_BALL)
+                .setName(name)
+                .setLore(lore)
+                .build();
+    }
+
+
+    public ItemStack freezeGun(String name, String... lore) {
+        return ItemBuilder
+                .newBuilder(Material.DIAMOND_HOE)
+                .setName(name)
+                .setLore(lore)
+                .build();
+    }
+
+
+    public ItemStack explosiveBow(String name, String... lore) {
+        return ItemBuilder
+                .newBuilder(Material.BOW)
+                .setName(name)
+                .setLore(lore)
+                .build();
+    }
+
+
+    public ItemStack antiBuilder(String name, String... lore) {
         return null;
     }
 
 
-    public ItemStack rocketLauncher() {
+    public ItemStack teleportationBall(String name, String... lore) {
         return null;
     }
 
 
-    public ItemStack swapperAxe() {
+    public ItemStack teleportationBow(String name, String... lore) {
         return null;
     }
 
 
-    public ItemStack timeWarp() {
+    public ItemStack angelGuardian(String name, String... lore) {
         return null;
     }
 
 
-    public ItemStack belchBomb() {
+    public ItemStack tntRain(String name, String... lore) {
         return null;
     }
 
 
-    public ItemStack freezeGun() {
+    public ItemStack invisibilityRaid(String name, String... lore) {
         return null;
     }
 
 
-    public ItemStack explosiveBow() {
+    public ItemStack focusMode(String name, String... lore) {
         return null;
     }
 
 
-    public ItemStack antiBuilder() {
+    public ItemStack fakePearl(String name, String... lore) {
         return null;
     }
 
 
-    public ItemStack teleportationBall() {
+    public ItemStack antiFall(String name, String... lore) {
         return null;
     }
 
 
-    public ItemStack teleportationBow() {
+    public ItemStack effectDisabler(String name, String... lore) {
         return null;
     }
 
 
-    public ItemStack angelGuardian() {
+    public ItemStack antiTrapBeacon(String name, String... lore) {
         return null;
     }
 
 
-    public ItemStack tntRain() {
+    public ItemStack loggerBait(String name, String... lore) {
         return null;
     }
 
 
-    public ItemStack invisibilityRaid() {
-        return null;
-    }
-
-
-    public ItemStack focusMode() {
-        return null;
-    }
-
-
-    public ItemStack fakePearl() {
-        return null;
-    }
-
-
-    public ItemStack antiFall() {
-        return null;
-    }
-
-
-    public ItemStack effectDisabler() {
-        return null;
-    }
-
-
-    public ItemStack antiTrapBeacon() {
-        return null;
-    }
-
-
-    public ItemStack loggerBait() {
-        return null;
-    }
-
-
-    public ItemStack explosiveEgg() {
+    public ItemStack explosiveEgg(String name, String... lore) {
         return null;
     }
 }
