0be0c326e4fdb5bbf9f8c537a968a2ce66224fcf
vCamilx <72714741+vCamilx@users.noreply.github.com>
2021-05-13 14:35:39 -0500

    comments were added as java docs

--- api/src/main/java/org/neptuneclub/abilities/api/service/IService.java
+++ api/src/main/java/org/neptuneclub/abilities/api/service/IService.java
@@ -1,7 +1,12 @@
 package org.neptuneclub.abilities.api.service;
 
 public interface IService {
+
+    /** Method for initiating all services. */
+
     void start();
+
+    /** Method to shut down / interrupt all services. */
 
     void interrupt();
 }
