847690385647d4bce61786bcf2a76f93b27e111f
vCamilx <72714741+vCamilx@users.noreply.github.com>
2021-05-14 16:33:52 -0500

    new test listener

--- plugin/src/main/java/org/neptuneclub/abilities/service/sub/ListenerService.java
+++ plugin/src/main/java/org/neptuneclub/abilities/service/sub/ListenerService.java
@@ -5,6 +5,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.neptuneclub.abilities.Abilities;
 import org.neptuneclub.abilities.api.service.IService;
+import org.neptuneclub.abilities.listener.JoinPlayerEvent;
 
 import java.util.Arrays;
 
@@ -13,8 +14,15 @@ public class ListenerService implements IService {
 
     private Abilities abilities;
 
+    private JoinPlayerEvent joinPlayerEvent;
+
     public void start() {
-        registerListener();
+        registerListener(
+                joinPlayerEvent
+        );
+        Bukkit
+                .getLogger()
+                .info("All listeners has been enabled");
     }
 
     public void interrupt() {
