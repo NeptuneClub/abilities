90906e1613538e40866c10fede48390f525e3d1f
vCamilx <72714741+vCamilx@users.noreply.github.com>
2021-05-14 16:31:20 -0500

    added credits and fixed an issue

--- api/src/main/java/org/neptuneclub/abilities/api/nms/NMSHandler.java
+++ api/src/main/java/org/neptuneclub/abilities/api/nms/NMSHandler.java
@@ -2,10 +2,22 @@ package org.neptuneclub.abilities.api.nms;
 
 import org.bukkit.inventory.ItemStack;
 
+/*
+ * Credits: Special thanks to Javier (https://github.com/Espryth)
+ * from there I got the name of the packages / classes and guided me in most of the NMS,
+ * besides this thanks to iAlexT (https://github.com/iAlexT)
+ * who was the one who explained me about packets and other things...
+ *
+ * Here https://www.spigotmc.org/threads/nms-tutorials-1-introduction-to-nms.204127/
+ * I found an nms tutorial, which helped me to create them.
+ *
+ * Sincerely yours: vCamilo
+ */
+
 public interface NMSHandler {
 
     /**
-     * <h3>Items builder</h1>
+     * <h3>Items builder</h3>
      *
      * <p> Here, everything will return the value of the specified item converted into an ItemStack, for the versioning
      * of the different versions 1.8x up to 1.16.5. </p>
