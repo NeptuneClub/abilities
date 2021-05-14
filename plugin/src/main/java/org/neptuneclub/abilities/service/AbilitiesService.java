f570532e21e2eb6a77226e9acbf1ed6fca510a29
vCamilx <72714741+vCamilx@users.noreply.github.com>
2021-05-14 16:32:59 -0500

    new loader service

--- plugin/src/main/java/org/neptuneclub/abilities/service/AbilitiesService.java
+++ plugin/src/main/java/org/neptuneclub/abilities/service/AbilitiesService.java
@@ -11,8 +11,12 @@ public class AbilitiesService implements IService {
     @Named("listener-service")
     private IService listenerService;
 
+    @Named("loader-service")
+    private IService loaderService;
+
     public void start() {
         listenerService.start();
+        loaderService.start();
     }
 
     public void interrupt() {
